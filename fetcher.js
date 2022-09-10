be3d6483b69f100e2dc86f79a738ff3a2184d3df
Edlyn Shih <edlyn0418@gmail.com>
2022-09-10 15:21:11 -0400

    Refactor write file function.

--- fetcher.js
+++ fetcher.js
@@ -4,16 +4,19 @@ const fs = require('fs'); //Use Node's fs (file system) module to write the file
 let url = process.argv[2]; //command line first argument
 let path = process.argv[3]; //command line second argument
 
-request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body', body); // Print the HTML for the url.
+request(url, (error, _unused, body) => {
+  if (error) {
+    console.log('error:', error.message); // Print the error if one occurred
+    return;
+  }
   
   fs.writeFile(path, body, err => {
     if (err) {
-      console.error(err);
-    } else {
-      console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
-    }
+      console.error(err.message);
+      return;
+    } 
+    //file successfully written
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+    
   });
 });
